f8363f7e0e3062d4f51183dc8eabb86c4ad2c4e5
Nayra Ferreira Lisboa <rebecanayra27@gmail.com>
2022-09-26 17:04:56 -0300

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,7 +6,44 @@ namespace Prep2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Prep 2");
+            Console.Write("What is your grade percent? ");
+            string input = Console.ReadLine();
+            int grade = int.Parse(input);
+
+            string letter = "";
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80)
+                {
+                    letter = "B";
+                }
+            else if (grade >= 70)
+                {
+                    letter = "C";
+                }
+            else if (grade >= 60)
+                {
+                    letter = "D";
+                }
+            else
+            {
+                letter = "F";
+            }
+
+            Console.WriteLine($"Your grade is {letter}.");
+
+            if (grade >= 70)
+            {
+                Console.WriteLine("You passed. Congratulations!");
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry. Better luck next time.");
+            }
         }
     }
 }
