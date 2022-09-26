7315cf2046b44a42504d8ba006ac27bc5bcec494
Nayra Ferreira Lisboa <rebecanayra27@gmail.com>
2022-09-26 18:36:14 -0300

    Tic Tac Toe Code

--- developer/Unit01/Program.cs
+++ developer/Unit01/Program.cs
@@ -1,12 +1,130 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Unit01
 {
     class Program
     {
         static void Main(string[] args)
+        { 
+            Console.WriteLine("Welcome! This is the Tic Tac Toe game!");
+            List<string> board = GetNewBoard();
+            string currentPlayer = "X";
+
+            while (!IsGameOver(board))
+            {
+                DisplayBoard(board);
+
+                int choice = GetMoveChoice(currentPlayer);
+                MakeMove(board, choice, currentPlayer);
+
+                currentPlayer = GetNextPlayer(currentPlayer);
+            }
+
+            DisplayBoard(board);
+            Console.WriteLine("Good game. Thanks for playing!");
+            Console.Write("Would you like to play again? ");
+            string answer = Console.ReadLine();
+                while (answer != "yes")
+                {
+                    break;
+                }
+        }
+        static List<string> GetNewBoard()
         {
-            Console.WriteLine("Hello World!");
+            List<string> board = new List<string>();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                board.Add(i.ToString());
+            }
+
+            return board;
+        }
+
+        static void DisplayBoard(List<string> board)
+        {
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+        }
+
+        static bool IsGameOver(List<string> board)
+        {
+            bool isGameOver = false;
+
+            if (IsWinner(board, "X") || IsWinner(board, "O") || IsTie(board))
+            {
+                isGameOver = true;
+            }
+
+            return isGameOver;
+        }
+
+        static bool IsWinner(List<string> board, string player)
+        {
+
+            bool isWinner = false;
+
+            if ((board[0] == player && board[1] == player && board[2] == player)
+                || (board[3] == player && board[4] == player && board[5] == player)
+                || (board[6] == player && board[7] == player && board[8] == player)
+                || (board[0] == player && board[3] == player && board[6] == player)
+                || (board[1] == player && board[4] == player && board[7] == player)
+                || (board[2] == player && board[5] == player && board[8] == player)
+                || (board[0] == player && board[4] == player && board[8] == player)
+                || (board[2] == player && board[4] == player && board[6] == player)
+                )
+            {
+                isWinner = true;
+            }
+
+            return isWinner; 
+        }
+
+        static bool IsTie(List<string> board)
+        {
+            bool foundDigit = false;
+
+            foreach (string value in board)
+            {
+                if (char.IsDigit(value[0]))
+                {
+                    foundDigit = true;
+                    break;
+                }
+            }
+
+            return !foundDigit;
+        }
+
+        static string GetNextPlayer(string currentPlayer)
+        {
+            string nextPlayer = "X";
+
+            if (currentPlayer == "X")
+            {
+                nextPlayer = "O";
+            }
+
+            return nextPlayer;
+        }
+
+        static int GetMoveChoice(string currentPlayer)
+        {
+            Console.Write($"{currentPlayer}'s turn to choose a spot (1-9): ");
+            string move_string = Console.ReadLine();
+
+            int choice = int.Parse(move_string);
+            return choice;
+        }
+
+        static void MakeMove(List<string> board, int choice, string currentPlayer)
+        {
+            int index = choice - 1;
+            board[index] = currentPlayer;
         }
     }
 }
