2e06527f28b498790fa9d1eb5c3224b9e44176f2
Nayra Ferreira Lisboa <rebecanayra27@gmail.com>
2022-09-26 17:12:58 -0300

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace Prep3
+namespace solo_prep_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Prep 3");
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Great! You guessed it!");
+                }
+
+            }        
         }
     }
 }
