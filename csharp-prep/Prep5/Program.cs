87d85733362e9c3e8381901ed515f9d97ee26b32
Nayra Ferreira Lisboa <rebecanayra27@gmail.com>
2022-09-26 17:55:45 -0300

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -8,6 +8,47 @@ namespace Prep5
         static void Main(string[] args)
         {
             Console.WriteLine("This is Prep 5");
+
+            DisplayWelcome();
+
+            string inputName = PromptUserName();
+
+            int inputNumber = PromptUserNumber();
+
+            int squaredNumber = SquareNumber(inputNumber);
+
+            DisplayResult (inputName, squaredNumber);
+
+
+            static void DisplayWelcome()
+            {
+                Console.WriteLine("Welcome to the program!");
+            }
+
+            static string PromptUserName()
+            {
+                Console.WriteLine("What is your name? ");
+                string name = Console.ReadLine();
+                return name;
+            }
+            
+            static int PromptUserNumber()
+            {
+                Console.WriteLine("Please enter your favorite number: ");
+                int number = int.Parse(Console.ReadLine());
+                return number;
+            }
+            
+            static int SquareNumber(int number)
+            {
+                int square = number * number;
+                return square;
+            }
+
+            static void DisplayResult(string name, int square)
+            {
+                Console.WriteLine($"{name}, the square of your number is {square}.");
+            }
         }
     }
 }
